66adb34b3b72efa3dc249d031580eccc7457f281
Neo <Neo@WIN-D2LLM3B5VG6>
2021-03-04 17:30:08 +0300

    first commit

---
+++ constants/tabs.js
@@ -0,0 +1,14 @@
+export default tabs = {
+  blogs: [
+    { id: 'popular', title: 'Popular' },
+    { id: '', title: 'Beauty' },
+    { id: 'stuff', title: 'Stuff' },
+    { id: 'clothes', title: 'Clothes'}
+  ],
+  stuff: [
+    { id: 'shoes', title: 'Shoes', },
+    { id: 'blogs', title: 'blogs', },
+    { id: 'stuff', title: 'Stuff', },
+    { id: 'places', title: 'Places', }
+  ],
+}
